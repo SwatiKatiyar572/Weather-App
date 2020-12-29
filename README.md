ca7f54ca80b8c8a1dd7539546ed5aa08cc4512a4
Swati Katiyar <70045942+SwatiKatiyar572@users.noreply.github.com>
2020-12-29 18:17:21 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Weather-App
-This is a weather app in react js using Weatherstack API to give the weather information of that particular location and also use the geolocation API to get our current position.
+# README #
+
+This README would normally document whatever steps are necessary to get your application up and running.
+
+### What is this repository for? ###
+
+* Quick summary
+* Version
+* [Learn Markdown](https://bitbucket.org/tutorials/markdowndemo)
+
+### How do I get set up? ###
+
+* Summary of set up
+* Configuration
+* Dependencies
+* Database configuration
+* How to run tests
+* Deployment instructions
+
+### Contribution guidelines ###
+
+* Writing tests
+* Code review
+* Other guidelines
+
+### Who do I talk to? ###
+
+* Repo owner or admin
+* Other community or team contact
---
+++ precache-manifest.dd18c9825902a35a9cdeda88fad986e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2b6d96cfe5d7597ae8a64d6632864a",
+    "url": "/Bishwahangdewan/Weather-App/index.html"
+  },
+  {
+    "revision": "317d39594e27248236ab",
+    "url": "/Bishwahangdewan/Weather-App/static/css/main.c6426f34.chunk.css"
+  },
+  {
+    "revision": "0236fcfd64f75c5bf844",
+    "url": "/Bishwahangdewan/Weather-App/static/js/2.9cd449f1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Bishwahangdewan/Weather-App/static/js/2.9cd449f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "317d39594e27248236ab",
+    "url": "/Bishwahangdewan/Weather-App/static/js/main.9e493017.chunk.js"
+  },
+  {
+    "revision": "577ab63d266c291e92ef",
+    "url": "/Bishwahangdewan/Weather-App/static/js/runtime-main.3eb1cdb3.js"
+  }
+]);
